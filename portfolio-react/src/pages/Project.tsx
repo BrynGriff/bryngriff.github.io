8c4a4f7bd157963b833fe33ab2e35f15954692d8
GriffLadd <tnzgriff@gmail.com>
2022-09-09 03:12:06 +1200

    Main projects running on react

--- portfolio-react/src/pages/Project.tsx
+++ portfolio-react/src/pages/Project.tsx
@@ -2,37 +2,124 @@ import React from 'react';
 import '../App.css';
 import {projectData, miniProjectData} from '../projects.js';
 import {Contact} from '../App';
+import { basename } from 'node:path/win32';
+
+interface Project{
+  name: string;
+  fullName: string;
+  downloadLink: string;
+  content: ProjectParagraph[];
+}
+
+interface ProjectParagraph{
+  header: string;
+  paragraph: string;
+}
 
 function Project() {
-  let queryString = window.location.search;
+  const json = JSON.parse(projectData);
+
+  let queryString: string = window.location.search;
   queryString = queryString.substring(1);
 
+  let projectObject: Project;
   if (queryString == "")
   {
       queryString = "0";
   }
 
-  console.log(queryString);
+  projectObject = json.find((element: Project) => element.name == queryString);
+  console.log(projectObject.fullName);
+  console.log(projectObject.content);
+
   return (
     <div className="App">
       
-      <ProjectBody/>
+      <ProjectBody name={projectObject.fullName} downloadUrl={projectObject.downloadLink} content={ProjectContent(projectObject)} bannerPath = {`images/banners/` + projectObject.name + `.png`}/>
       <Contact />
     </div>
   );
 }
 
-function ProjectBody()
+interface DownloadButtonProps {
+  url:string;
+}
+function DownloadButton({url}:DownloadButtonProps)
+{
+  return(
+    <a id="download-link" target="_blank" href={url}>
+      <div id="download-button">Download</div>
+    </a>
+  )
+}
+
+function ProjectContent(projectObject: Project)
+{
+  console.log(projectObject.fullName);
+
+  return(
+    <>
+      <div id="project-body-images">
+        <ProjectImages projectObject={projectObject}/>
+      </div>
+
+      <div id = "project-body-text">              
+        <ProjectText projectObject={projectObject}/>
+      </div>
+    </>
+  );
+}
+
+interface ProjectObjectProps{
+  projectObject: Project;
+}
+function ProjectImages({projectObject} : ProjectObjectProps)
+{
+  const images = [];
+  for (let i = 0; i < 4; i++)
+  {
+    images.push(
+    <img src={require(`../images/previews/${projectObject.name}/${i + 1}.png`)}></img>
+    );
+  }
+  return (<>{images}</>);
+}
+
+interface ProjectObjectProps{
+  projectObject: Project;
+}
+function ProjectText({projectObject} : ProjectObjectProps)
+{
+  const text = [];
+  for (let i = 0; i < projectObject.content.length; i++)
+  {
+    text.push(
+      <>
+      <h1>{projectObject.content[i].header}</h1>
+      {projectObject.content[i].paragraph}
+      {i != projectObject.content.length - 1 && <div className='between-line'/>}
+      </>
+    );
+  }
+  return (<>{text}</>);
+}
+
+interface ProjectBodyProps {
+  name: string;
+  downloadUrl?: string;
+  content?: JSX.Element;
+  bannerPath: string;
+}
+function ProjectBody({name= "Test", downloadUrl="", content, bannerPath} : ProjectBodyProps)
 {
 return(
   <>
-  <div id="project-background">
-
+  <div id="project-background" style={{backgroundImage: `url(${require("../" + bannerPath)})`}}>
   </div>
   <div id = "project-body">
-    <div id="project-banner">
+    <div id="project-banner" style={{backgroundImage: `url(${require("../" + bannerPath)})`}}>
       <div id="project-banner-name">
-
+        <h1>{name}</h1>
       </div>
     </div>
     <div id="download-bar">
@@ -40,18 +127,10 @@ return(
         <a href="/">
             <div id="back-button">Home</div>
         </a>
-        <a id="download-link" target="_blank">
-            <div id="download-button">Download</div>
-        </a>
+        {downloadUrl != "" && <DownloadButton url={downloadUrl}/>}
       </div>
     </div>
-    <div id="project-body-images">
-
-    </div>
-
-    <div id = "project-body-text">              
-      
-    </div>
+    {content}
   </div>
   </>
 );
